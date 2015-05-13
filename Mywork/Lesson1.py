51173c88982d15d88726c4b3c4bf8c1b5e1f20ae
blaiseyuri <blaiseyuri@ufl.edu>
2015-05-12 23:47:15 -0400

    Created chart showing Fico and interest rates

--- Mywork/Lesson1.py
+++ Mywork/Lesson1.py
@@ -1,4 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
 
+
+# we have to clean up the raw data set which we will do
+# in the next lesson. But for now let's look at the cleaned up data.
+# import the cleaned up dataset into a pandas data frame
 df = pd.read_csv('../datasets/loanf.csv')
-print df
+
+# extract FICO Score and Interest Rate and plot them
+# FICO Score on x-axis, Interest Rate on y-axis
+intrate = df['Interest.Rate']
+fico = df['FICO.Score']
+p = plt.plot(fico,intrate,'o')
+ax = plt.gca()
+xt = ax.set_xlabel('FICO Score')
+yt = ax.set_ylabel('Interest Rate %')
+plt.show()
