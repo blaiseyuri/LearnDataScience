a9960a6cae2e6153f74c0473fc7af933043bdcd4
blaiseyuri <blaiseyuri@ufl.edu>
2015-05-26 00:22:31 -0400

    Finished All Lessons and removed all commvscrime files.

--- Mywork/CrimeFactors.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('../datasets/CommvsCrime.csv')
-goodCrime = df['ViolentCrimesPerPop']
-badCrime = df['nonViolPerPop']
-totalCrime = badCrime + goodCrime
-print totalCrime
